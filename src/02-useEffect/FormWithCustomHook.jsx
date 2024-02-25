44486b39ec29267781143774154162a03b70a73b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-24 21:19:17 -0500

    feat (FormWithCustomhook.jsx): add component
    Use custom hook to handle form state

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,44 @@
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+
+    const { formState, onInputChange } = useForm({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const { username, email, password } = formState;
+
+    return (
+        <>
+            <h1>Form with Custom Hook</h1>
+            <hr />
+            <input
+                type="text"
+                className="form-control"
+                placeholder="username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="email"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+            <input
+                type="password"
+                className="form-control mt-2"
+                placeholder="contraseña"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+            />
+
+        </>
+    );
+};
