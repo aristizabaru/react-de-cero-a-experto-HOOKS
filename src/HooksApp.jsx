423d649dcb4efec0dd4c9070ef3fdbad7f218759
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-24 21:16:18 -0500

    feat (HooksApp.jsx): add FormWithCustomHook component

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,9 +1,10 @@
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 
 export const HooksApp = () => {
     return (
-        <SimpleForm />
+        <FormWithCustomHook />
     );
 };
