0f2f025f5234b20106ba53317351dae005d6e93b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 08:52:36 -0500

    feat (LoadingMessage.jsx): implement component
    Handle copy and style for loading message

---
+++ src/03-examples/LoadingMessage.jsx
@@ -0,0 +1,10 @@
+export const LoadingMessage = () => {
+    return (
+        <section
+            style={{ height: 200 }}
+            className="alert alert-info text-center p-o m-0 d-flex align-items-center justify-content-center"
+        >
+            <h2>Loading...</h2>
+        </section>
+    );
+};
