ec62456aa7fc3c16aba96017c1c111d8ebcdce6c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 08:55:12 -0500

    feat (MultipleCustomHooks.jsx): add pagination to load multiple Pokemon's
    Handle useFetch and useCounter hooks to load multiple Pokemon's through pagination

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,46 @@
-import { useFetch } from "../hooks";
+import { useCounter, useFetch } from "../hooks";
+import { LoadingMessage } from "./LoadingMessage";
+import { PokemonCard } from "./PokemonCard";
 
 export const MultipleCustomHooks = () => {
 
-    const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/4');
+    const { counter, decrement, increment } = useCounter(1);
+    const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
 
     return (
         <>
             <h1>Información del Pokemón</h1>
             <hr />
-            {isLoading && <p>Cargando..</p>}
+            {
+                isLoading
+                    ? <LoadingMessage />
+                    : (
+                        <PokemonCard
+                            id={counter}
+                            name={data.name}
+                            sprites={[
+                                data.sprites.front_default,
+                                data.sprites.front_shiny,
+                                data.sprites.back_default,
+                                data.sprites.back_shiny,
+                            ]}
+                        />
+                    )
+            }
             <h2>{data?.name}</h2>
+
+            <button
+                className="mt-3"
+                onClick={() => counter > 1 ? decrement() : null}
+            >
+                Anterior
+            </button>
+            <button
+                className="mt-3"
+                onClick={() => increment()}
+            >
+                Siguiente
+            </button>
         </>
 
     );
