e73352d5654e910b898890c89672d85205b68dae
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-24 19:39:04 -0500

    feat: add main component to try hooks

---
+++ src/HooksApp.jsx
@@ -0,0 +1,8 @@
+// import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+
+export const HooksApp = () => {
+    return (
+        <CounterWithCustomHook />
+    );
+};
