659c1e6a8d18410e8c932990e23ee56e2d6a713b
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 11:44:49 -0500

    reafactor (HookApp.jsx): render Layout component instead of FocusRef

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -3,10 +3,11 @@
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusRef } from "./04-useRef/FocusRef";
+// import { FocusRef } from "./04-useRef/FocusRef";
+import { Layout } from "./05-useLayoutEffect/Layout";
 
 export const HooksApp = () => {
     return (
-        <FocusRef />
+        <Layout />
     );
 };
