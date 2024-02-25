43a62dd5ee4abcfb292e3f11c8425c8c6f1b4667
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 10:50:20 -0500

    feat (HooksApp.jsx): add FocusRef component

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -2,10 +2,11 @@
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusRef } from "./04-useRef/FocusRef";
 
 export const HooksApp = () => {
     return (
-        <MultipleCustomHooks />
+        <FocusRef />
     );
 };
