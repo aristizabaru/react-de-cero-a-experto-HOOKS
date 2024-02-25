a8cae93d284e6e8baf3bcb41c0bc5ceda8b40785
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 07:56:18 -0500

    feat (HooksApp.jsx): add MultipleCustomHooks component

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,10 +1,11 @@
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 
 export const HooksApp = () => {
     return (
-        <FormWithCustomHook />
+        <MultipleCustomHooks />
     );
 };
