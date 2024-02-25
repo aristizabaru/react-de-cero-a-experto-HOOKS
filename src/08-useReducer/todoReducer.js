f75e2b8ba6a40bdf2e444767b8c841f4572d76b9
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 17:57:31 -0500

    feat (todoReducer.js): implements todoReducer boilerplate

---
+++ src/08-useReducer/todoReducer.js
@@ -0,0 +1,10 @@
+export const todoReducer = (initialState = [], action) => {
+    switch (action.type) {
+        case 'ABC':
+
+            return initialState;
+
+        default:
+            return initialState;
+    }
+};
