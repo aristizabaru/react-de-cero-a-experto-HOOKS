c2073aad6c7c44896a8a8f4427b530227fc2a7c5
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-24 20:28:21 -0500

    feat (HooksApp.jsx): add SimpleForm functional component

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,8 +1,9 @@
 // import { CounterApp } from "./01-useState/CounterApp";
-import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 export const HooksApp = () => {
     return (
-        <CounterWithCustomHook />
+        <SimpleForm />
     );
 };
