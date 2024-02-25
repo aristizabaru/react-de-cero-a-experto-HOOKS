ce288e909c6f9b86507b6eaaab7c1221083d171a
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 08:51:48 -0500

    refactor (useFetch.js)

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,6 +10,41 @@ export const useFetch = (url) => {
     });
 
     useEffect(() => {
+
+        const getFetch = async () => {
+
+            setLoadingState();
+
+            const resp = await fetch(url);
+
+            // Sleep (to test loader)
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+            if (!resp.ok) {
+                setState({
+                    data: null,
+                    isLoading: false,
+                    hasError: true,
+                    error: {
+                        code: resp.status,
+                        message: resp.statusText,
+                    }
+                });
+
+                return;
+            }
+
+            const data = await resp.json();
+            setState({
+                data: data,
+                isLoading: false,
+                hasError: false,
+                error: null
+            });
+
+            console.log({ data });
+        };
+
         getFetch();
 
     }, [url]);
@@ -22,41 +57,6 @@ export const useFetch = (url) => {
             error: null
         });
     };
-
-    const getFetch = async () => {
-
-        setLoadingState();
-
-        const resp = await fetch(url);
-
-        // Sleep (to test loader)
-        await new Promise(resolve => setTimeout(resolve, 2000));
-
-        if (!resp.ok) {
-            setState({
-                data: null,
-                isLoading: false,
-                hasError: true,
-                error: {
-                    code: resp.status,
-                    message: resp.statusText,
-                }
-            });
-
-            return;
-        }
-
-        const data = await resp.json();
-        setState({
-            data: data,
-            isLoading: false,
-            hasError: false,
-            error: null
-        });
-
-        console.log({ data });
-    };
-
 
     return {
         data: state.data,
