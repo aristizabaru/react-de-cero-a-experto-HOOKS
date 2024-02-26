f1bd53c3b763a5cef39382efa1eaaf319bc8cfd5
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 20:13:57 -0500

    feat (TodoItem.jsx): add onDeleteTodo prop
    Handles the emission of the event for deleting an item

--- src/08-useReducer/TodoItem.jsx
+++ src/08-useReducer/TodoItem.jsx
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types';
 
-export const TodoItem = ({ value }) => {
+export const TodoItem = ({ value, id, onDeleteTodo }) => {
     return (
         <li className="list-group-item d-flex justify-content-between align-items-center">
             <span>{value}</span>
-            <button className="small">Borrar</button>
+            <button
+                className="small"
+                onClick={() => onDeleteTodo(id)}
+            >Borrar</button>
         </li>
     );
 };
 
 TodoItem.propTypes = {
     value: PropTypes.string,
+    id: PropTypes.number,
+    onDeleteTodo: PropTypes.func
 };
 
