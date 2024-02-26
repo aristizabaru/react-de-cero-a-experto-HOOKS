3e96b6e81aa121f023b14c0b3efe202c412244b7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 19:45:48 -0500

    feat (TodoApp.jsx): add data persistence with localStorage API

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,24 +1,18 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 import { TodoAdd } from "./TodoAdd";
 
-const initialState = [
-    {
-        id: new Date().getTime(),
-        todo: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        todo: 'Recolectar la piedra del destino',
-        done: false,
-    }
-];
+const initReducer = () => JSON.parse(localStorage.getItem('todos')) ?? [];
 
 export const TodoApp = () => {
 
-    const [todos, dispatchTodo] = useReducer(todoReducer, initialState);
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], initReducer);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
 
     const handleNewTodo = (description) => {
 
