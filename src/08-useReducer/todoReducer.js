76a5749363325b54f2fd5ab5ac37752405e60505
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 20:46:40 -0500

    feat (todoReducer.jsx): add toggle todo use case

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -2,8 +2,15 @@ export const todoReducer = (initialState = [], action) => {
     switch (action.type) {
         case '[TODO] create todo':
             return [...initialState, action.payload];
+
         case '[TODO] delete todo':
             return initialState.filter(todo => todo.id !== action.payload);
+
+        case '[TODO] toggle todo':
+            return initialState.map(todo => {
+                if (todo.id === action.payload) return { ...todo, done: !todo.done };
+                return todo;
+            });
 
         default:
             return initialState;
