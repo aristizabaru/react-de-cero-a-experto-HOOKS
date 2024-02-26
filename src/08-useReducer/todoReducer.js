8a6b110838f1d2e42dcbf02227cdaeb994903ff1
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 20:11:46 -0500

    feat (todoReducer.js): implement delete todo case

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,8 +1,9 @@
 export const todoReducer = (initialState = [], action) => {
     switch (action.type) {
-        case '[TODO] add todo':
-
+        case '[TODO] create todo':
             return [...initialState, action.payload];
+        case '[TODO] delete todo':
+            return initialState.filter(todo => todo.id !== action.payload);
 
         default:
             return initialState;
