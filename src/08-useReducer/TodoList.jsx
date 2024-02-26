d3020872ea26b85d1864f9be49cabe7afa7009b1
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 20:12:19 -0500

    feat (TodoList.jsx): add onDeleteTodo prop

--- src/08-useReducer/TodoList.jsx
+++ src/08-useReducer/TodoList.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 import { TodoItem } from './TodoItem';
 
-export const TodoList = ({ items = [] }) => {
+export const TodoList = ({ items = [], onDeleteTodo }) => {
     return (
         <ul className="list-group">
             {
-                items.map(({ id, todo }) => <TodoItem key={id} value={todo} />)
+                items.map(({ id, todo }) => (
+                    <TodoItem
+                        key={id}
+                        value={todo}
+                        id={id}
+                        onDeleteTodo={onDeleteTodo} />
+                ))
             }
         </ul>
     );
@@ -13,4 +19,5 @@ export const TodoList = ({ items = [] }) => {
 
 TodoList.propTypes = {
     items: PropTypes.array,
+    onDeleteTodo: PropTypes.func
 };
