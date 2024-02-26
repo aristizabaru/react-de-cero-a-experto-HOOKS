419ef7db456811c7930a2992b2b6e00989f71742
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-25 20:50:34 -0500

    feat (TodoApp.jsx): add handleToggleTodo function
    Dispatch payload to the reducer to change the done property of the TODO item

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -40,6 +40,15 @@ export const TodoApp = () => {
         dispatchTodo(action);
     };
 
+    const handleToggleTodo = (id) => {
+        const action = {
+            type: '[TODO] toggle todo',
+            payload: id
+        };
+
+        dispatchTodo(action);
+    };
+
     return (
         <>
             <h2>Todo APP</h2>
@@ -47,7 +56,7 @@ export const TodoApp = () => {
             <div className="row">
                 <div className="col-7">
                     <h3 className="mb-3">TODOS (10) <small style={{ fontSize: '0.6em', marginLeft: 8 }}>pendientes (5)</small></h3>
-                    <TodoList items={todos} onDeleteTodo={handleDeleteTodo} />
+                    <TodoList items={todos} onDeleteTodo={handleDeleteTodo} onToggleTodo={handleToggleTodo} />
                 </div>
                 <div className="col-5">
                     <h3 className="mb-3">Agregar TODO</h3>
